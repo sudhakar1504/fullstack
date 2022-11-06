9001104d43094b5ab97a3368a242dffde01566aa
sudhakar1504 <sudhakarkumar1504@gmail.com>
2022-11-06 12:42:13 +0530

    port change

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from "express";
 
 const App = express()
 App.use(express.json());
-
+const port = 3000
 
 App.get("/",(req,res)=>{
 res.json("response here")
 })
 
-App.listen(3030,()=>{
+App.listen(process.env.PORT || port,()=>{
     console.log('backend connected');
 })
